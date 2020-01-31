121eed8d4c1f0b719e9d8f670f43eb2ac067c1e0
Lwamtekie <ltekie87@yahoo.com>
2020-01-30 18:44:09 -0600

    added a single page for product

--- beautifulmess.ui/src/Components/Products/Products.js
+++ beautifulmess.ui/src/Components/Products/Products.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
-import getProducts from '../../Helpers/Data/ProductData';
+import ProductData from '../../Helpers/Data/ProductData';
 import ProductCard from '../ProductCard/ProductCard';
 import './Products.scss';
 
@@ -11,20 +11,20 @@ class Product extends React.Component {
   }
 
   componentDidMount() {
-    getProducts()
+    ProductData.getProducts()
       .then(res => this.setState({ Products: res }));
-    console.error('product');
   }
+
 
   render() {
     const { Products } = this.state;
-    const printProducts = Products.map(product => <ProductCard
+    const printProducts = Products.map(product => <ProductCard key={product.id}
         product={product}
       />);
     return (
      <div className="Product">
         <div className="home-Center">
-        <ButtonGroup className="filterButton animated bounce delay-0.5s">
+        <ButtonGroup className="filterButton">
         <Button id="0" onClick={this.filterProducts}>All</Button>
         <Button id="1" onClick={this.filterProducts}>Industrial</Button>
         <Button id="2" onClick={this.filterProducts}>FarmHouse</Button>
