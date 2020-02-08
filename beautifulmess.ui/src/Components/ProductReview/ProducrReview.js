e861a4f76d31d24113b71f01e309a87ebf0816e9
Lwamtekie <ltekie87@yahoo.com>
2020-02-08 14:17:04 -0600

    wip

--- beautifulmess.ui/src/Components/ProductReview/ProducrReview.js
+++ beautifulmess.ui/src/Components/ProductReview/ProducrReview.js
@@ -1,53 +1,20 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import ProductReviewData from '../../Helpers/Data/ProductReviewData';
-import UserData from '../../Helpers/Data/UserData';
 import './ProductReview.scss';
 
 
 class ProductReview extends React.Component {
- state = {
-   productreview: [],
-   rating: '',
-   comment: '',
-   userName: '',
- }
-
-
-getProductReview = () => {
-  ProductReviewData.getProductsReview()
-    .then(res => this.setState({ productreview: res }));
-};
-
-ComponentDidMount = () => {
-  this.getProductReview();
-  const userSessionInfo = UserData.getSessionUser(); // How to get userInfo from sessionStorage
-  this.setState({ userInfo: userSessionInfo });
-};
-
-render() {
-  const { productreview } = this.props;
-  return (
+  render() {
+    const productreview = this.props.productReview;
+    return (
         <div className="ProductReview">
-            <div className="row">
-            <form className="comment-form form-group" onSubmit={this.handleSubmit}>
-        <div className="input-group">
-          <span className="input-group-addon">Name</span>
-          <input type="text" placeholder="Your name" className="form-control" />
+          <h1>UserComment</h1>
+          <div className="input-group-addon">Name:{productreview.userName}</div>
+          <div className="input-group-addon">Rating:{productreview.rating}</div>
+          <div className="input-group-addon">Comment:{productreview.comment}</div>
         </div>
-        <div className="input-group">
-          <span className="input-group-addon">Comment</span>
-          <input type="text" placeholder="Say something..." className="form-control" />
-        </div>
-        <input type="submit" value="Post" className="btn btn-primary" />
-      </form>
     );
   }
-});
-        </div>
-        </div>
-  );
-}
 }
 
-export default withRouter(ProductReview);
+
+export default ProductReview;
--- beautifulmess.ui/src/Components/SingleProduct/SingleProduct.js
+++ beautifulmess.ui/src/Components/SingleProduct/SingleProduct.js
@@ -1,8 +1,11 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProductData from '../../Helpers/Data/ProductData';
 import UserProductData from '../../Helpers/Data/UserProductData';
 import UserData from '../../Helpers/Data/UserData';
+import ProductReviewData from '../../Helpers/Data/ProductReviewData';
+import ProductReview from '../ProductReview/ProducrReview';
 
 
 import './SingleProduct.scss';
@@ -10,6 +13,43 @@ import './SingleProduct.scss';
 class SingleProduct extends React.Component {
  state = {
    product: {},
+   productReview: [],
+   rating: '',
+   comment: '',
+   userName: '',
+ }
+
+ userInputHandler = (e) => {
+   const { value } = e.target;
+   const InputName = e.target.name;
+   this.setState({ [InputName]: value });
+ }
+
+AddReview = (e) => {
+  e.preventDefault();
+  const userSessionInfo = UserData.getSessionUser();
+  const tempReview = {
+    rating: this.state.rating,
+    comment: this.state.comment,
+    userName: userSessionInfo.userName,
+  };
+
+  ProductReviewData.postNewProductReview(tempReview)
+    .then(() => {
+      this.setState({
+        rating: '',
+        comment: '',
+        userName: '',
+      });
+      this.props.getProductReview();
+    })
+    .catch(error => console.error(error));
+}
+
+
+ getProductReview = () => {
+   ProductReviewData.getProductsReview(this.state.product.id)
+     .then(res => this.setState({ productReview: res }));
  }
 
  addMyProducts = () => {
@@ -29,18 +69,21 @@ class SingleProduct extends React.Component {
  getProduct = () => {
    const productId = this.props.match.params.id;
    ProductData.getSingleProduct(productId)
-     .then(productPromise => this.setState({ product: productPromise.data }))
+     .then((productPromise) => {
+       this.setState({ product: productPromise.data });
+       this.getProductReview();
+     })
      .catch(err => (err));
  }
 
  componentDidMount() {
    this.getProduct();
-   console.error('product');
  }
 
-
  render() {
+   const { rating, comment, userName } = this.state;
    const { product } = this.state;
+   const BuildproductReview = this.state.productReview.map(productReview => <ProductReview productReview={productReview}/>);
    const ProductsLink = '/products';
    return (
     <div className="ProductCard col-4">
@@ -52,10 +95,28 @@ class SingleProduct extends React.Component {
         <img src={product.imageUrl} className="card-img" alt="..." />
         <Link className="btn btn-success" to={ProductsLink}>Return</Link>
         <button className="btn btn-success" onClick={this.addMyProducts}>Add</button>
-        < getgetProductReview getProductReview={this.props.getProductReview}/>
+        </div>
       </div>
-      </div>
-    </div>
+        {BuildproductReview}
+        <form>
+  <div class="form-group">
+    <label for="exampleInputName">Name</label>
+    <input name="userName" type="name" class="form-control" id="exampleInputName" aria-describedby="NameHelp" value={userName} onChange={this.userInputHandler}/>
+    <small id="nameHelp" class="form-text text-muted">We'll  share your name.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputComment">Comment</label>
+    <input name="comment" type="Comment" class="form-control" id="exampleInputComment"aria-describedby="CommentHelp" value={comment} onChange={this.userInputHandler}/>
+    <small id="nameHelp" class="form-text text-muted">We'll  share your comment.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputRating">Rating</label>
+    <input name="rating" type="rating" class="form-control" id="exampleInputRating" aria-describedby="RatingHelp" value= {rating} onChange={this.userInputHandler}/>
+    <small id="RatingHelp" class="form-text text-muted">We'll  share your rating.</small>
+  </div>
+  <button type="AddReview" class="btn btn-primary">AddReview</button>
+</form>
+</div>
    );
  }
 }
